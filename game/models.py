653c60734ae0fc82cfa31e0dffa99972a2bd54ee
Tea_ <vero.zero.xxx@gmail.com>
2023-05-02 17:10:27 +0900

    css 입력

--- game/models.py
+++ game/models.py
@@ -1,15 +1,5 @@
 from django.db import models
-#from main.models import Main_User
 
-# # Create your models here.
-# class User_Score(models.Model):
-#     nickname = models.ForeignKey(Main_User,on_delete=models.CASCADE, db_column='nickname')
-#     Score = models.IntegerField()
-
-#     class Meta:
-#         db_table = 'User_Score'
-#         managed = True
-        
 class MyModel(models.Model):
     img = models.ImageField(upload_to='images/', default=0)
     name = models.TextField(max_length=20,default=0)
--- game/views.py
+++ game/views.py
@@ -23,20 +23,13 @@ def startgame(request):
 
         return redirect('/end/endgame/')
     
-    #User_Score.objects.create(nickname=Main_User.nickname)
-    # DB에서 이미지 목록 가져오기
+
+    # DB에서 이미지 목록 가져오기 
     items =MyModel.objects.all()
     img_list = [item.img.url for item in items]
-    selected_imgs = random.sample(set(img_list), 20)
+    selected_imgs = random.sample(set(img_list), 40)#랜덤문으로 40개의 수량만 확보 후 리스트로 재정렬
     context = {'selected_imgs':selected_imgs}
 
-    #user_input_name = request.POST.get('img_name')
-    # score = Main_User.objects.latest('nickname').score
-    #for  selected_img in enumerate(selected_imgs):
-    #    context['selected_img'] = selected_img
-    #user = Main_User.objects.latest('nickname')
-    #user.score = score
-    #user.save()
     return render(request, 'game/game.html', context)
 
 
