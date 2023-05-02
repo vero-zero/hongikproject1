3a931d40663ed52df4e00d6aea7b60ba0bf554b8
Tea_ <vero.zero.xxx@gmail.com>
2023-05-02 13:06:34 +0900

    페이징연결

--- end/views.py
+++ end/views.py
@@ -3,7 +3,7 @@ from main.models import Main_User
 # Create your views here.
 
 def endgame(request):
-    userscore =  Main_User.objects.get(nickname_list)
-    nickname_list =  [ni.nickname for ni in userscore]
-    score_list = [ sc.score for sc in userscore]
-    return render (request, 'end/end.html' , {'nickname_list':nickname_list})
+    lastuser =  Main_User.objects.latest('id')
+    lastnick = lastuser.nickname
+    lastscore = lastuser.score
+    return render (request, 'end/end.html' , {'lastnick':lastnick,'lastscore':lastscore})
