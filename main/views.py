aeb185080e7dd5044e3dc8a7aeb7af452329e4e2
Tea_ <vero.zero.xxx@gmail.com>
2023-05-02 13:56:56 +0900

    DB 정리

--- main/views.py
+++ main/views.py
@@ -4,14 +4,14 @@ from .models import Main_User
 
 def startmain(request):
     if request.method == 'POST':
-        nickname = request.POST.get('nickname')
-        if nickname != "":
-            if Main_User.objects.filter(nickname=nickname).exists():
+        name = request.POST.get('nickname')
+        u = Main_User()
+        u.nickname = name
+        u.save()
+        if name != "":
+            if Main_User.objects.filter(nickname=name).exists():
                 messages.error(request,'이미 사용중인 닉네임입니다.')
                 return redirect('/main/startmain/?msg=1')        
-            u = Main_User()
-            u.nickname = nickname
-            u.save()
             return redirect('/game/startgame/',{})
         return render(request, 'main/main.html', {})
     return render(request, 'main/main.html', {})
