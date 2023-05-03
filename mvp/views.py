50d8c84aef22156ea8d9f6db6d2faaee5df62a13
Tea_ <vero.zero.xxx@gmail.com>
2023-05-03 12:33:22 +0900

    완성본

--- mvp/views.py
+++ mvp/views.py
@@ -3,7 +3,7 @@ from main.models import Main_User
 # Create your views here.
 
 def mvplist(request):
-    mvplist = Main_User.objects.filter(score=10)
+    mvplist = Main_User.objects.filter(score = 10)
     print(len(mvplist))
     return render (request,'mvp/mvp.html',{'mvplist':mvplist})
 
