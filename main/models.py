5235b10befe1631e45ebb8792285cc39887567ee
Tea_ <vero.zero.xxx@gmail.com>
2023-05-05 18:00:13 +0900

    삭제

--- main/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Main_User(models.Model):
-    nickname = models.CharField(max_length=20)
-    score = models.IntegerField(default=0)
-    class Meta:
-        db_table = 'Main_User' #연결할 테이블 명 
-        managed = True #데이터
-    
--- mvp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render,HttpResponse
-from main.models import Main_User
-# Create your views here.
-
-def mvplist(request):
-    mvplist = Main_User.objects.filter(score = 10)
-    print(len(mvplist))
-    return render (request,'mvp/mvp.html',{'mvplist':mvplist})
-
