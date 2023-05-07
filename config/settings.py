2aa9df4c60f06a51e525ea38104c1cf8b7d6a854
Tea_ <vero.zero.xxx@gmail.com>
2023-05-07 15:33:05 +0900

    anywhere 연결

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-^#n*4dn+3=%qq_qb&iv4mvj4#9zm@vc&70blve!_)_eh@p9!pe"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['teako.pythonanywhere.com']
 
 # Application definition
 DEBUG = False
